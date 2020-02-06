7d8fa25fb74ad0e62a4908ef06c6ea9e4b9240cb
Connor Munro <hello@cbmunro.com>
2020-02-06 18:05:10 -0500

    WIP Add pathfinding

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const express = require('express')
 const logger = require('morgan')
 const app = express()
 
-// const path = require('ngraph.path')
-// const pathFinder = path.aGreedy(graph)
-
 const {
   fallbackHandler,
   notFoundHandler,
   genericErrorHandler,
   poweredByHandler
 } = require('./handlers.js')
+
+const createGraph = require('ngraph.graph')
+const graph = createGraph()
 
 // For deployment to Heroku, the port needs to be set using ENV, so
 // we check for the port number in process.env
@@ -24,6 +24,21 @@ app.use(bodyParser.json())
 app.use(poweredByHandler)
 
 // --- SNAKE LOGIC GOES BELOW THIS LINE ---
+const plotFood = points => {
+  let count = 0
+  points.forEach(point => {
+    graph.addNode(`food-${count}`, point);
+    count++
+  })
+}
+
+const plotMe = points => {
+  let count = 0
+  points.forEach(point => {
+    graph.addNode(`me-${count}`, point);
+    count++
+  })
+}
 
 // Handle POST request to '/start'
 app.post('/start', (request, response) => {
@@ -32,6 +47,8 @@ app.post('/start', (request, response) => {
   // Response data
   const data = {
     color: '#DFFF00',
+    headType: 'bendr',
+    tailType: 'pixel'
   }
 
   return response.json(data)
@@ -40,7 +57,13 @@ app.post('/start', (request, response) => {
 // Handle POST request to '/move'
 app.post('/move', (request, response) => {
   // NOTE: Do something here to generate your move
-  console.log(request);
+  plotFood(request.board.food)
+  plotMe(request.you)
+
+  const pathFinder = path.aStar(graph)
+  const foundPath = pathFinder.find('me', 'food-1')
+  console.log(foundPath[0])
+
 
   // Response data
   const data = {
